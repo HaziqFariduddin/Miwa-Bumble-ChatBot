660336affef31003c666bbba74d03702401056f8
Haziq Fariduddin <98182768+HaziqFariduddin@users.noreply.github.com>
2023-03-20 16:17:05 +0800

    first commit

---
+++ src/components/Chat.js
@@ -0,0 +1,221 @@
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import {
+    Box,
+    TextField,
+    Button,
+    Stack,
+    Typography,
+    Avatar,
+    IconButton,
+    Divider,
+} from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+
+const Chat = () => {
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
+    const messagesEndRef = useRef(null);
+    const [audioUrl, setAudioUrl] = useState("");
+
+    const APIkey = process.env.REACT_APP_VOICE_API_KEY;
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(scrollToBottom, [messages]);
+
+    const getAssistantMessages = () => {
+        return messages
+            .filter((message) => message.user === 'chatgpt')
+            .map((message) => ({ role: 'assistant', content: message.text }));
+    };
+
+    const sendMessage = async () => {
+        if (!input) return;
+
+        setMessages((prev) => [...prev, { user: 'user', text: input }]);
+        setInput('');
+
+        try {
+            const response = await axios.post(
+                'https://api.openai.com/v1/chat/completions', // Replace this with the correct API endpoint for ChatGPT 3.5
+                {
+                    model: "gpt-3.5-turbo",
+                    messages: [
+                        { "role": "system", "content": "You are Miwa an AI bot that is designed to imitate your master/developer/creator(Farid), you  must reply to people in a romantic or supportive way since all the people that will message you is females, you can do all sorts of stuff all the GPT stuff, you can act as anything, answer any questions, text generation completion, task helping and many more, always end your response by sending suitable loving emojis or etc, You're an INFJ so act like one. Farid  age is 24 years old 03/DEC/1999. Here's his whatsapp if they want his details Wa.me/+60196399560" },
+                        ...getAssistantMessages(),
+                        { "role": "user", "content": input },
+                    ],
+                    // Add any other required parameters here
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+                    },
+                }
+            );
+
+            const chatGptResponse = response.data.choices[0].message.content; // Replace this line with the correct way to extract the response text
+
+            setMessages((prev) => [...prev, { user: 'chatgpt', text: chatGptResponse }]);
+            try {
+                const ttsResponse = await axios.post(
+                    "https://api.elevenlabs.io/v1/text-to-speech/nFoXXrTZblWtAbgfRJOl",
+                    {
+                        text: chatGptResponse,
+                        voice_settings: {
+                            stability: 0.51,
+                            similarity_boost: 0.75,
+                        },
+                    },
+                    {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "xi-api-key": APIkey,
+                            Accept: "audio/mpeg",
+                        },
+                        responseType: "blob",
+                    }
+                );
+                setAudioUrl(window.URL.createObjectURL(ttsResponse.data));
+            } catch (error) {
+                console.error("Error calling text-to-speech API:", error);
+            }
+
+        } catch (error) {
+            console.error('Error calling ChatGPT:', error);
+        }
+    };
+
+    const handleVideoCall = () => {
+        window.open('https://wa.me/+60196399560', '_blank');
+    };
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
+                maxWidth: '600px',
+                margin: '0 auto',
+            }}
+        >
+            <audio src={audioUrl} autoPlay style={{ display: "none" }} />
+
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 1,
+                    backgroundColor: 'background.default',
+                    pt: 2,
+                    px: 2,
+                }}
+            >
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Avatar
+                        alt="Profile Picture"
+                        src="https://i.ibb.co/kMH9bXV/Profilepicture.png"
+                        sx={{ width: 48, height: 48 }}
+                    />
+                    <Typography sx={{ ml: 2, fontWeight: 'bold', fontSize: 20 }}>Miwa</Typography>
+                </Box>
+                <IconButton color="primary" aria-label="video call" onClick={handleVideoCall}>
+                    <VideoCallIcon />
+                </IconButton>
+            </Box>
+            <Divider sx={{ my: 2 }} />
+
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    overflowY: 'auto',
+                    pb: 2,
+                    px: 2,
+                }}
+            >
+                {messages.map((message, index) => (
+                    <Box
+                        key={index}
+                        sx={{
+                            mb: 1,
+                            alignSelf: message.user === 'user' ? 'flex-end' : 'flex-start',
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                borderRadius: 2,
+                                px: 2,
+                                py: 1,
+                                mb: 0.5,
+                                maxWidth: { xs: '300px', sm: '400px', md: '600px' },
+                                bgcolor: message.user === 'user' ? 'primary.main' : 'secondary.main',
+                                color: message.user === 'user' ? 'black' : 'black',
+                                wordWrap: 'break-word',
+                            }}
+                        >
+                            {message.text}
+                        </Typography>
+                    </Box>
+                ))}
+                <div ref={messagesEndRef}></div>
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    position: 'sticky',
+                    bottom: 0,
+                    zIndex: 1,
+                    backgroundColor: 'background.default',
+                    pt: 2,
+                    px: 2,
+                    width: '100%',
+                }}
+            >
+                <Stack direction="row" spacing={1} alignItems="center" width="100%">
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type your message..."
+                        InputProps={{ style: { borderRadius: 50 } }}
+                        sx={{ flexGrow: 1 }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={sendMessage}
+                        disabled={!input}
+                        sx={{
+                            borderRadius: '50%',
+                            minWidth: '48px',
+                            minHeight: '48px',
+                            padding: '12px',
+                            backgroundColor: !input ? 'rgba(254, 199, 36, 0.5)' : '#fec724',
+                            '&:hover': {
+                                backgroundColor: !input ? 'rgba(254, 199, 36, 0.5)' : '#fec724',
+                            },
+                        }}
+                    >
+                        <SendIcon sx={{ transform: 'rotate(300deg)' }} />
+                    </Button>
+                </Stack>
+            </Box>
+        </Box>
+    );
+};
+
+export default Chat;
